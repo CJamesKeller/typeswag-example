3b1a7d17a757d6fc29fefe72a27abcf4f0231bea
Chris Keller <cjameskeller@gmail.com>
2019-04-12 16:06:53 -0500

    Correct structure

--- index.ts
+++ index.ts
@@ -1,33 +1,31 @@
-import * as cors from 'cors';
-import { Route } from 'typeswag';
-import { EventEmitter } from 'events';
-import { JsonController, Get, UseBefore, InternalServerError } from 'routing-controllers';
+import 'module-alias/register';
+import 'reflect-metadata';
+import './controllers/example/index.ts';
+import * as express from 'express';
+import * as helmet from 'helmet';
+import { Express } from 'express';
+import { useExpressServer } from 'routing-controllers';
+import { generateSwagger } from './generateSwagger';
+import { CustomErrorHandler } from './ErrorHandler';
+import { Example } from '@controllers/index';
 
-const myEmitter = new EventEmitter();
+const port = process.env.PORT || 3000;
 
-const corsOpts = { optionsSuccessStatus: 200 }; // for IE11
+const app: Express = express();
+app.set('port', port);
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-const example = 'example';
-
-@Route('Example')
-@JsonController('/api/v1')
-@UseBefore(cors(corsOpts))
-export class Example {
-    @Get(`/${example}`)
-    [example] = async (): Promise<void | {}> => {
-        try {
-            await setTimeout(() => this.handleData('Here is the data'), 100);
-            return await new Promise(resolve => myEmitter.on('gotData', (data) => resolve(data)));
-        } catch (err) {
-            throw new InternalServerError('Oh no!');
-        }
-    }
-
-    handleData = (data: string): void => {
-        if (data) {
-            myEmitter.emit('gotData', data);
-            return;
-        }
-    }
+const serverConfig = {
+  controllers: [Example],
+  defaultErrorHandler: false,
+  middlewares: [CustomErrorHandler]
 }
 
+useExpressServer(app, serverConfig);
+generateSwagger();
+
+app.listen(port, () => console.info(`Listening on port ${port}`));
+
+export { app };
